434d9d54ab38b445f6a2fc8d946c503b19ed7221
Mc Francis Estoque Ancla <anclamcfrancis037@gmail.com>
2025-03-25 07:59:58 +0800

    Update ARRAYS_ANCLA.java

--- ARRAYS_ANCLA.java
+++ ARRAYS_ANCLA.java
@@ -1,42 +1,17 @@
 
-//package ancla_midterm;
-//
-//
-//public class ARRAYS_ANCLA {
-//    public static void main(String[] args){
-//        
-//        String[] array = {"A","B","C","D","E","F","G"};
-//        
-//        for (String letter : array)
-//            
-//        System.out.println(letter + ",");
-//           
-//    }
-//    
-//}
-//
-////public class Multi_dimentional {
-//    public static void main(String[] args){
-//        
-//        int[][] ancla = {
-//            {1,2,3,4},
-//            {5,6,7,8},
-//            {9,10,11,12}
-//        };
-//        
-//        for (int i = 0; i < ancla.length; i++){
-//            
-//            System.out.print("The Elements in row " + (i + 1) + " : ");
-//        
-//        for (int j = 0; j < ancla[i].length; j++) 
-//        {
-//        
-//        System.out.print(ancla[i][j] + " ");
-//        }
-//        
-//        System.out.println();
-//        
-//    }
-//            
-//    } 
-//}
+package ancla_midterm;
+
+
+public class ARRAYS_ANCLA {
+   public static void main(String[] args){
+       
+       String[] array = {"A","B","C","D","E","F","G"};
+       
+       for (String letter : array)
+           
+       System.out.println(letter + ",");
+          
+   }
+   
+}
+
